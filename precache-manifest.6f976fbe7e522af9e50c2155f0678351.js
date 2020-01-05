de3b5095656103c196b0916d3fa87a3b1cd0f494
APFirebolt <aspper20@gmail.com>
2020-01-05 12:51:42 -0800

    Updates

---
+++ precache-manifest.6f976fbe7e522af9e50c2155f0678351.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c19786c6cb60c3859f770a9458dc22d8",
+    "url": "/React-Employee-Management/index.html"
+  },
+  {
+    "revision": "d7bf431134b58468b95a",
+    "url": "/React-Employee-Management/static/css/2.70d9d258.chunk.css"
+  },
+  {
+    "revision": "6f7718e718d7f0a91174",
+    "url": "/React-Employee-Management/static/css/main.8a972705.chunk.css"
+  },
+  {
+    "revision": "d7bf431134b58468b95a",
+    "url": "/React-Employee-Management/static/js/2.d452514f.chunk.js"
+  },
+  {
+    "revision": "78ddf5a465591a03b9601ba2ddd63415",
+    "url": "/React-Employee-Management/static/js/2.d452514f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6f7718e718d7f0a91174",
+    "url": "/React-Employee-Management/static/js/main.7c6c5369.chunk.js"
+  },
+  {
+    "revision": "4e5adde9a8139033b3b8",
+    "url": "/React-Employee-Management/static/js/runtime-main.d7b889a1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-Employee-Management/static/media/logo.5d5d9eef.svg"
+  }
+]);
